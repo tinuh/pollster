8d8d0ace0cc999eb06ef6263bf295b812afadd3b
Linkai <linkai861@gmail.com>
2021-03-20 01:16:48 -0400

    fix fontawesome size

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import App from './App';
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "./lib/theme";
 import { AuthProvider } from './lib/auth';
+import "@fortawesome/fontawesome-svg-core/styles.css";
 //import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
