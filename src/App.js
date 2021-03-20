1e18d66a9dadc095fc7652e35e77b4ef8e131524
tinu4 <tinuvanapamula@gmail.com>
2021-03-19 21:28:27 -0400

    smal changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Dashboard from "./pages/dashboard";
 //import { ChakraProvider } from "@chakra-ui/react"
 
 function App() {
   return (
     <div className="App">
-        <h1>Pollster</h1>
+      <Router>
+        <Switch>
+          <Route path = "/" component = {Dashboard}></Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/dashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Dashboard() {
+    return (
+        <div>
+            <center>
+                <h1>Pollster</h1>
+            </center>
+        </div>
+    )
+}
