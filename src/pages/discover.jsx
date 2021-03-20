eac927f489da88c37d482429f9b30da3e7e3a515
Anurag Gowda <gowdaanuragr@gmail.com>
2021-03-20 11:03:21 -0400

    defaultmap

--- src/pages/discover.jsx
+++ src/pages/discover.jsx
@@ -22,18 +22,9 @@ export default function Discover(){
             <Heading as="h1" ml="46%">Map!</Heading>
 
             <Box w="90%" h="50vw" borderWidth="1px" borderRadius="lg" overflow="hidden">
-                <LoadScript
-                    googleMapsApiKey="AIzaSyA7igErPwTsDQt3kXKO-e2k8PNWDVvoRaA"
-                >
-                    <GoogleMap
-                    mapContainerStyle={containerStyle}
-                    center={center}
-                    zoom={10}
-                    >
-                    { /* Child components, such as markers, info windows, etc. */ }
-                    <></>
-                    </GoogleMap>
-                </LoadScript>
+                <Map defaultCenter={[50.879, 4.6997]} defaultZoom={12} width="100%" height="100%">
+                    <Marker anchor={[50.874, 4.6947]} payload={1} onClick={({ event, anchor, payload }) => {}} />
+                </Map>
             </Box>
             <Heading mt="2vw" ml="2vw">Top rated polls near you: </Heading>
             <Box style={{"display":"inline-flex"}} mb="1vw" mt="1vw">
