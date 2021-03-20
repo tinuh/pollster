90ea864a465655a08f5acd29a7200dc115164b13
Anurag Gowda <gowdaanuragr@gmail.com>
2021-03-19 22:49:12 -0400

    poll

--- src/components/poll.jsx
+++ src/components/poll.jsx
@@ -4,6 +4,7 @@ import { ChevronUpIcon, ChevronDownIcon } from '@chakra-ui/icons';
 import { Text } from "@chakra-ui/react";
 import { Heading } from "@chakra-ui/react";
 import { Button } from "@chakra-ui/react";
+import { Grid, GridItem } from "@chakra-ui/react"
 
 import Pollpopup from './pollPopup';
 
@@ -12,13 +13,26 @@ export default function Poll(props){
     const [showModal, setShowModal] = React.useState(false);
 
     return (
-        <Box maxW="sm" borderWidth="1px" borderRadius="lg" overflow="hidden">
-            <Box>
-                <ChevronUpIcon /> <Text>5{/*props.upvotes*/}</Text> <ChevronDownIcon />
-            </Box>
-            <Heading> {props.pollName} </Heading>
-            <Text> {props.pollDescription} </Text>
-            <Button variant="ghost" onClick={()=>setShowModal(true)}>Open Poll</Button>
+        <Box maxW="sm" borderWidth="1px" borderRadius="lg" overflow="hidden" width="300px">
+            <Grid
+                h="200px"
+                templateRows="repeat(3, .3fr)"
+                templateColumns="repeat(5, .1fr)"
+                gap={4}
+                >    
+                <GridItem rowSpan={2} colSpan={1}>
+                    <ChevronUpIcon /> <Text>5{/*props.upvotes*/}</Text> <ChevronDownIcon />
+                </GridItem>
+                <GridItem colSpan={4}>
+                    <Heading as="h6" size="md"> Poll title {/*props.pollName*/} </Heading>
+                </GridItem>
+                <GridItem colSpan={4}>
+                    <Text> Basic poll description {/*props.pollDescription*/} </Text>
+                </GridItem>
+                <GridItem colSpan={5}>
+                    <Button colorScheme="blue" onClick={()=>setShowModal(true)} isFullWidth={true}>Open Poll</Button>
+                </GridItem>
+            </Grid>
             {showModal && <Pollpopup data={props.popupInfo} set={setShowModal} />}
         </Box>
     )
