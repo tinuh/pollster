699ddac7be2c1a948e04d7f39ff034f890f29c9f
Anurag Gowda <gowdaanuragr@gmail.com>
2021-03-20 01:44:38 -0400

    pfp links

--- src/pages/profilepage.jsx
+++ src/pages/profilepage.jsx
@@ -8,16 +8,24 @@ import { Text } from "@chakra-ui/react";
 import { Button } from "@chakra-ui/react"
 import { useToast } from "@chakra-ui/react";
 import Poll from "../components/poll";
+import { Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton} from "@chakra-ui/react";
+import { Input } from "@chakra-ui/react";
 
 export default function Profile(props){
 
     const [edit, setEdit] = React.useState(false);
     const [displayName, setDisplayName] = React.useState('example'); //get from firebase
     const [desc, setDesc] = React.useState("Example description... blah blah");
+    const [showIconModal, setShowIconModal] = React.useState(false);
+    const [pfpLink, setPfpLink] = React.useState('');
     const toast = useToast()
 
     function saveData(){
         setEdit(false);
+    }
+
+    function savePfpLink(){
+        setShowIconModal(false); // and save it to firebase
     }
 
     function handleDispChange(e){
@@ -39,6 +47,25 @@ export default function Profile(props){
 
     return (
         <Box mt="2vw">
+
+            <Modal isOpen={showIconModal} onClose={()=>setShowIconModal(false)}>
+                <ModalOverlay />
+                <ModalContent>
+                <ModalHeader>{/*props.data.pollname*/}</ModalHeader>
+                <ModalCloseButton />
+                <ModalBody>
+                    {/*props.data.form*/}
+                    <Input placeholder="link to profile pic" value={pfpLink} onChange={(e)=>setPfpLink(e.target.value)} />
+                </ModalBody>
+    
+                    <ModalFooter>
+                        <Button colorScheme="blue" mr={3} onClick={savePfpLink}>
+                            Submit
+                        </Button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
+
             <Heading ml="34%">Your Profile</Heading>
             <Grid
                 borderWidth="1px" borderRadius="lg" overflow="hidden"
@@ -51,7 +78,7 @@ export default function Profile(props){
                 mt="1vw"
             >
                 <GridItem rowSpan={2} colSpan={2}>
-                    <Avatar size="2xl" src="" mt="2vw" ml="4vw" />
+                    <Avatar size="2xl" src="" mt="2vw" ml="4vw" onClick={()=>setShowIconModal(true)} />
                 </GridItem>
                 <GridItem rowSpan={1} colSpan={3} mt="2vw">
                     <div style={{"display":"inline-flex"}}>
