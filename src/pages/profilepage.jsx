ea5b1f260916d51d726cc887adc2a420a6fc3159
Anurag Gowda <gowdaanuragr@gmail.com>
2021-03-19 23:58:00 -0400

    Create profilepage.jsx

---
+++ src/pages/profilepage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function ProfilePage(){
+    
+    const loggedIn = true // <= this is the dummy data
+
+    return (
+        <Box>
+            
+        </Box>
+    )
+}
