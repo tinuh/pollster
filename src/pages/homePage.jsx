41170441bbd266dc86745f389baf6b5f264ea53c
Linkai <linkai861@gmail.com>
2021-03-20 15:49:33 -0400

    add clean homepage :DDD

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -1,30 +1,91 @@
 import React from 'react';
-import { Container, Text, Button, Heading } from "@chakra-ui/react";
+import {
+    Box,
+    Container,
+    SimpleGrid,
+    Heading,
+    Text,
+    Button,
+    Image,
+    Center,
+    Link as ChakraLink,
+} from '@chakra-ui/react';
 import { DownloadIcon } from '@chakra-ui/icons';
 
 export default function homePage(){
     return (
         <>
-            <Heading>POLLSTER</Heading>
-            <Text>A polling app with unlimited potential</Text>
-            <Container align="center" display="inline-flex">
-                <img src='/hompagesvg/create.svg' alt="create" w="20%" h="20%" />
-                <Text ml="1vw" align="left">Teachers, government officials, and even concerned citizens can lanuch polls about problems they are facing to get opinions on the best method to apprach the problem. Or, anyone can create polls just for fun! </Text>
+            <Box py={20} px={10}
+                textAlign="center"
+            >
+                <Heading as="h1" size="2xl" m={2}>Pollster</Heading>
+                <Heading as="h2" size="lg" fontWeight="normal">
+                    A community polling app with unlimited potential
+                </Heading>
+                <Button my={8} colorScheme="blue" size="lg">Start discovering today</Button>
+            </Box>
+
+            <Container maxW="container.lg" p={12}>
+                <SimpleGrid columns={{ base: 1, md: 2 }} spacing={12}>
+                <Box>
+                    <Center>
+                        <Image m={4} src='/hompagesvg/create.svg' alt="create" h={48}/>
+                    </Center>
+                    <Heading as="h3" size="lg" my={2}>For everyone, and anyone.</Heading>
+                    <Text fontSize="lg">
+                        Teachers, members of the government, and even concerned citizens can lanuch polls about problems they are facing to get opinions on the best method to apprach the problem. Or, anyone can create polls just for fun!
+                    </Text>
+                </Box>
+                <Box>
+                    <Center>
+                        <Image m={4} src='/hompagesvg/options.svg' alt="create" h={48}/>
+                    </Center>
+                    <Heading as="h3" size="lg" my={2}>Customized and relevant.</Heading>
+                    <Text fontSize="lg">
+                        The community can pick and choose which polls are most important for them, this can allow the community to prioritze the most urget polls that could potentially cause drastic reforms.
+                    </Text>
+                </Box>
+                <Box>
+                    <Center>
+                        <Image m={4} src='/hompagesvg/map.svg' alt="create" h={48}/>
+                    </Center>
+                    <Heading as="h3" size="lg" my={2}>Built with community values in mind.</Heading>
+                    <Text fontSize="lg">
+                        Easily find polls near you. You can answer polls related to problems that are arising in your community that can help better the enviornment, aid in the growth of local startups, allow you to share your ideas with your local government, and so much more!
+                    </Text>
+                </Box>
+                <Box>
+                    <Center>
+                        <Image m={4} src='/hompagesvg/pwa.svg' alt="create" h={48}/>
+                    </Center>
+                    <Heading as="h3" size="lg" my={2}>Use Pollster anywhere!</Heading>
+                    <Text fontSize="lg">
+                        Pollster is PWA optimized, so you can download it and run it as if it were a native app!
+                    </Text>
+                </Box>
+                </SimpleGrid>
             </Container>
-            <Container align="center" display="inline-flex">
-                <Text ml="1vw" align="left">The community can pick and choose which polls are most important for them, this can allow the community to prioritze the most urget polls that could potentially cause drastic reforms. </Text>
-                <img src='/hompagesvg/options.svg' alt="options" w="20%" h="20%" />
+
+            <Container maxW="container.lg" p={12}>
+                <Heading as="h2" size="xl" my={2}
+                    color="brand.500"
+                >Get the app</Heading>
+                <Text fontSize="lg">
+                Download (or use the web version) Pollster today and create and answer polls to help your community
+                </Text>
+                <Button colorScheme="blue" variant="solid" my={4} leftIcon={<DownloadIcon />}>Download for mobile/desktop</Button>
             </Container>
-            <Container align="center" display="inline-flex">
-                <img src='/hompagesvg/map.svg' alt="map" w="20%" h="20%" /> 
-                <Text ml="1vw" align="left">Easily find polls near you. You can answer polls related to problems that are arising in your community that can help better the enviornment, aid in the growth of local startups, allow you to share your ideas with your local government, and so much more! </Text> 
+
+            <Container maxW="container.lg" p={12}>
+                <Heading as="h2" size="xl" my={2}
+                >Open source!</Heading>
+                <Text fontSize="lg">
+                Our source code is open to the public.
+                </Text>
+                <ChakraLink href="https://github.com/tinuh/pollster" target="_blank" style={{ textDecoration: 'none' }}>
+                    <Button colorScheme="gray" variant="solid" my={4} leftIcon={<DownloadIcon />}>GitHub repo</Button>
+                </ChakraLink>
             </Container>
-            <Container align="center" display="inline-flex">
-                <Text ml="1vw" align="left">Pollster is PWA optimized, so you can download it and run it as if it were a native app!</Text>
-                <img src='/hompagesvg/pwa.svg' alt="pwa" w="20%" h="20%" /> 
-            </Container>
-            <Text> Download (or use the web version) Pollster today and create and answer polls to help your community </Text>
-            <Button colorScheme="twitter" leftIcon={<DownloadIcon />}> DOWNLOAD </Button>
         </>
     )
 }
