d96631845c6437a2ca3509c023a28500d8df1287
Anurag Gowda <gowdaanuragr@gmail.com>
2021-03-20 10:09:43 -0400

    whats up

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -10,18 +10,7 @@ import Poll from '../components/poll';
 export default function Dashboard() {
     return (
         <>
-            <Box ml="10%">
-                <Heading as="h1" ml="46%">Map!</Heading>
-                {/* PUT THE MAP IN THIS BOX */}
-                <Box w="90%" h="50vw" borderWidth="1px" borderRadius="lg" overflow="hidden"></Box>
-                <Heading mt="2vw" ml="2vw">Top rated polls near you: </Heading>
-                <Box style={{"display":"inline-flex"}} mb="1vw" mt="1vw">
-                    <Poll pollvoting={true} /> <Box ml="1vw"></Box> <Poll pollvoting={true} /> <Box ml="1vw"></Box> <Poll pollvoting={true} /> <Box ml="1vw"></Box> <Poll pollvoting={true} />
-                </Box>
-                <Box style={{"display":"inline-flex"}} mb="5vw">
-                    <Poll pollvoting={true} /> <Box ml="1vw"></Box> <Poll pollvoting={true} /> <Box ml="1vw"></Box> <Poll pollvoting={true} /> <Box ml="1vw"></Box> <Poll pollvoting={true} />
-                </Box>
-            </Box>
+            
         </>
     )
 }
