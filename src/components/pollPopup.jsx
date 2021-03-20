16937948963037b255c975d5bd244171961df479
Anurag Gowda <gowdaanuragr@gmail.com>
2021-03-19 21:52:26 -0400

    basic

---
+++ src/components/pollPopup.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import {
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,} from "@chakra-ui/react"
+
+export default function Pollpopup(props){
+
+    const { isOpen, onOpen, onClose } = useDisclosure()
+
+    return (
+        <Modal isOpen={isOpen} onClose={onClose}>
+            <ModalOverlay />
+            <ModalContent>
+            <ModalHeader>{props.pollname}</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+                {props.form}
+            </ModalBody>
+
+                <ModalFooter>
+                    <Button colorScheme="blue" mr={3} onClick={onClose}>
+                        Submit
+                    </Button>
+                    <Button variant="ghost">Open in new page</Button>
+                </ModalFooter>
+            </ModalContent>
+        </Modal>
+    )
+}
