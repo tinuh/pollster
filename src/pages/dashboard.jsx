7b6bffb09bfd111cc394b2196700a447c6a21e80
Anurag Gowda <gowdaanuragr@gmail.com>
2021-03-20 19:08:19 -0400

    Update dashboard.jsx

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -48,7 +48,7 @@ export default function Dashboard() {
             <Heading size="lg" fontWeight="bold">My Polls</Heading>
             <SimpleGrid p={8} columns={{ base: 1, md: 2, lg: 4 }} spacing={6}>
                 {polls && polls.map(poll => 
-                    <Poll name={poll.name} description={poll.description} poll={poll} />
+                    <Poll name={poll.name} description={poll.description} data={poll} />
                 )}
             </SimpleGrid>
         </Container>
