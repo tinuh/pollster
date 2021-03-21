0d12fd4db8010fa4c49d477df6cb7f166c6052da
Linkai <linkai861@gmail.com>
2021-03-20 22:20:57 -0400

    finish text responses for poll results

--- src/pages/pollResults.jsx
+++ src/pages/pollResults.jsx
@@ -9,13 +9,8 @@ import {
     Stack
 } from "@chakra-ui/react";
 import { useParams } from 'react-router-dom';
-<<<<<<< HEAD
-import { getDoc, getUserFromRef } from '../lib/db';
+import { getDoc, getUserFromRef, getSubCol } from '../lib/db';
 import { Link } from 'react-router-dom';
-=======
-import { getDoc, getUserFromRef, getSubDoc, getSubCol } from '../lib/db';
-import {Link} from 'react-router-dom';
->>>>>>> 5556b4df09c5d20a225736bd56fc58eb431ba50b
 import { Map, Marker } from 'pigeon-maps';
 
 export default function PollResults(){
@@ -24,7 +19,7 @@ export default function PollResults(){
     const pollHasVotes = false;
     const [poll, setPoll] = React.useState(false);
     const [user, setUser] = React.useState(false);
-    const [responses, setResponses] = React.useState(false);
+    const [responses, setResponses] = React.useState([]);
     const getProvider = (x, y, z) => `https://cartodb-basemaps-a.global.ssl.fastly.net/light_all/${z}/${x}/${y}.png`;
 
     React.useEffect(() => {
@@ -57,7 +52,6 @@ export default function PollResults(){
     }, [id, toast])
 
     return(user ? (
-<<<<<<< HEAD
         <Container maxW="container.sm">
             <Heading as="h1" m={12} align="center">Poll Info</Heading>
             <Stack direction="column" spacing={8} mb={8}>
@@ -65,7 +59,7 @@ export default function PollResults(){
                     <Text>Name: {poll.name}</Text> 
                     <Text>Author: <Link to={"/profile/" + user.id}>{user.displayName}</Link></Text>
                     <Text>Description: {poll.description}</Text> 
-                    <Text>Responses: {} </Text> 
+                    <Text>Responses: {responses.length} </Text> 
                     {pollHasVotes && <Text>Poll votes: {2}</Text>}
                 </Box>
                 <Box p={8} borderWidth="1px" borderRadius="lg">
@@ -81,36 +75,11 @@ export default function PollResults(){
                             />
                         :
                             <Stack direction="column" spacing={2} maxH="100vh" overflow="auto">
-                                <Box m={1} py={4} px={6} borderWidth="1px" borderRadius="lg">
-                                    test
-                                </Box>
-                                <Box m={1} py={4} px={6} borderWidth="1px" borderRadius="lg">
-                                    test
-                                </Box>
-                                <Box m={1} py={4} px={6} borderWidth="1px" borderRadius="lg">
-                                    test
-                                </Box>
-                                <Box m={1} py={4} px={6} borderWidth="1px" borderRadius="lg">
-                                    test
-                                </Box>
-                                <Box m={1} py={4} px={6} borderWidth="1px" borderRadius="lg">
-                                    test
-                                </Box>
-                                <Box m={1} py={4} px={6} borderWidth="1px" borderRadius="lg">
-                                    test
-                                </Box>
-                                <Box m={1} py={4} px={6} borderWidth="1px" borderRadius="lg">
-                                    test
-                                </Box>
-                                <Box m={1} py={4} px={6} borderWidth="1px" borderRadius="lg">
-                                    test
-                                </Box>
-                                <Box m={1} py={4} px={6} borderWidth="1px" borderRadius="lg">
-                                    test
-                                </Box>
-                                <Box m={1} py={4} px={6} borderWidth="1px" borderRadius="lg">
-                                    test
-                                </Box>
+                                {responses.map(res =>
+                                    <Box m={1} py={4} px={6} borderWidth="1px" borderRadius="lg">
+                                        {res.answer}
+                                    </Box>
+                                )}
                             </Stack>
                         }
                     </Box>
@@ -122,34 +91,5 @@ export default function PollResults(){
                 </Box>
             </Stack>
         </Container>
-=======
-        <>  
-            <Heading align = "center" >Poll results</Heading>
-            <Text ml="35vw" mt="1vw">Poll info:</Text>
-            <Box ml="35vw" borderWidth="1px" borderRadius="lg" className="chart-container" style={{"position": "relative", "height":(pollHasVotes?"13vw":"11vw"), "width":"30vw"}}>
-                <Text ml="2vw" mt="1vw">Name: {poll.name}</Text> 
-                <Text ml="2vw" mt="1vw">Author: <Link to = {"/profile/" + user.id}>{user.displayName}</Link> </Text>
-                <Text ml="2vw" mt="1vw">Description: {poll.description}</Text> 
-                <Text ml="2vw" mt="1vw">Responses: {responses ? responses.length : 0} </Text> 
-                {/* {pollHasVotes && <Box><Text ml="2vw" mt="1vw">Poll votes: {2} </Text></Box> } */}
-            </Box>
-            <Text ml="35vw" mt="1vw">Responses:</Text>
-            <Box ml="35vw" borderWidth="1px" borderRadius="lg" className="chart-container" style={{"position": "relative", "height":"30vh", "width":"30vw", "paddingTop":".5vw", "paddingBottom":"1vw"}}>
-                <Doughnut
-                    data={{
-                        labels: ['Option 1', 'Option 2', 'Option 3', 'Option 4'],
-                        datasets: [{data:[1,2,3], backgroundColor: ['red', 'blue', 'orange']}]
-                    }}
-                    options={{ maintainAspectRatio: false }}
-                />
-            </Box>
-            <Text ml="35vw" mt="1vw">Location:</Text>
-            <Box ml="35vw" mb="5vw" borderWidth="1px" borderRadius="lg" className="chart-container" style={{"position": "relative", "height":"30vh", "width":"30vw"}}>
-                <Map defaultCenter={[poll.location._lat, poll.location._long]} defaultZoom={12} width="100%" height="100%" provider={getProvider}>
-                    <Marker anchor={[poll.location._lat, poll.location._long]}  width={50} height={50} />
-                </Map>
-            </Box>
-        </>
->>>>>>> 5556b4df09c5d20a225736bd56fc58eb431ba50b
     ) : (<Heading align = "center">Loading...</Heading>))
 }
